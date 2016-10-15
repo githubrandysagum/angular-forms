bd7339344c7de40371630a7e22888a8e1807bde1
Randy Sagum <randy.sagum@yahoo.com>
2016-10-14 18:04:19 -0700

    Added forms hero-form.component.ts and create a two way data binding

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,9 +1,22 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent }   from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HeroFormComponent } from './hero-form.component';
+
+
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  declarations: [
+    AppComponent,
+    HeroFormComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
+
+
+
 export class AppModule { }
